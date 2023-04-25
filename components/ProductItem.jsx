ad4572d98e98a5debc20b0f5a2de31b7540722a0
M0mahmoud <houdmohamed85@gmail.com>
2023-04-25 17:32:38 +0200

    Show Products Items

---
+++ components/ProductItem.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const ProductItem = ({ product }) => {
+  return (
+    <div className="card ">
+      <Link href={`/product/${product.slug}`} className="">
+        <Image
+          src={product.image}
+          alt={product.name}
+          width={350}
+          height={300}
+          className="rounded shadow  object-cover w-full"
+        />
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-lg">{product.name}</h2>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Layout from "@/components/Layout";
+import ProductItem from "@/components/ProductItem";
+import data from "@/utils/data";
 import React from "react";
 
 const index = () => {
   return (
     <Layout title={"Home"}>
-      <h1>Setupy</h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug} />
+        ))}
+      </div>
     </Layout>
   );
 };
