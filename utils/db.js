b2f683502deeecd11fff15bc4d5e61109a552893
M0mahmoud <houdmohamed85@gmail.com>
2023-04-29 21:52:46 +0300

    Adding DB

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const connection = {};
+
+async function connect() {
+  if (connection.icConnected) {
+    console.log("Already connected");
+    return;
+  }
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState;
+    if (connection.isConnected === 1) {
+      console.log("use previous connection");
+      return;
+    }
+  }
+
+  await mongoose.disconnect();
+
+  const db = await mongoose.connect(process.env.MONGODB_URL);
+  console.log("New connection");
+  connection.icConnected = db.connections[0].readyState;
+}
+
+// Disconnect
+async function disconnect() {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === "production") {
+      await mongoose.disconnect();
+      connection.isConnected = false;
+    } else {
+      console.log("Not Disconnected");
+    }
+  }
+}
+
+const db = { connect, disconnect };
+export default db;
